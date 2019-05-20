496f66087bba271a71ef2c84675cd4ee9f896cda
Tadi <TadiT7@github.com>
2019-05-20 19:14:50 +0000

    Add OnePlus7Pro-user 9 PKQ1.190110.001 1905150026 release-keys
    Signed-off-by: Tadi <TadiT7@github.com>

---
+++ system/system/xbin/nfc_trasmit_start.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+echo "seclet chip type"
+pnx --mcc 9 
+echo "nfc transmit start"
+pnx --frf 1
+echo "nfc transmit start end"
---
+++ system/system/xbin/nfc_trasmit_stop.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+echo "seclet chip type"
+pnx --mcc 9
+echo "nfc transmit stop"
+pnx --frf 0
+echo "nfc transmit stop end"
